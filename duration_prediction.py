fff83e9e73b0303deea7915e0b1c3f53846df890
Daniel Vargas <devg28@gmail.com>
2023-06-26 20:42:58 -0400

    initial commit for mlflow.

---
+++ duration_prediction.py
@@ -0,0 +1,144 @@
+"""Duration prediction with MLFlow."""
+import pandas as pd
+import pickle
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import Ridge
+
+from sklearn.metrics import mean_squared_error
+import mlflow
+
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, ExtraTreesRegressor
+from sklearn.svm import LinearSVR
+
+import xgboost as xgb
+from hyperopt import fmin, tpe, hp, STATUS_OK, Trials
+from hyperopt.pyll import scope
+
+mlflow.set_tracking_uri("sqlite:///mlflow.db")
+mlflow.set_experiment("nyc-taxi-experiment")
+
+
+def read_dataframe(filename):
+    df = pd.read_parquet(filename)
+
+    # print(df.head())
+
+    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+
+    df['duration'] = df.tpep_dropoff_datetime - df.tpep_pickup_datetime
+    df.duration = df.duration.apply(lambda td: td.total_seconds() / 60)
+
+    df = df[(df.duration >= 1) & (df.duration <= 60)]
+
+    categorical = ['PULocationID', 'DOLocationID']
+    df[categorical] = df[categorical].astype(str)
+
+    return df
+
+
+df_train = read_dataframe('./data/yellow_tripdata_2023-01.parquet')[0:100]
+df_val = read_dataframe('./data/yellow_tripdata_2023-02.parquet')[0:100]
+
+# len(df_train), len(df_val)
+# (73908, 61921)
+df_train['PU_DO'] = df_train['PULocationID'] + '_' + df_train['DOLocationID']
+df_val['PU_DO'] = df_val['PULocationID'] + '_' + df_val['DOLocationID']
+categorical = ['PU_DO']  # 'PULocationID', 'DOLocationID']
+numerical = ['trip_distance']
+
+dv = DictVectorizer()
+
+train_dicts = df_train[categorical + numerical].to_dict(orient='records')
+X_train = dv.fit_transform(train_dicts)
+
+val_dicts = df_val[categorical + numerical].to_dict(orient='records')
+X_val = dv.transform(val_dicts)
+target = 'duration'
+y_train = df_train[target].values
+y_val = df_val[target].values
+lr = LinearRegression()
+lr.fit(X_train, y_train)
+
+y_pred = lr.predict(X_val)
+
+mean_squared_error(y_val, y_pred, squared=False)
+
+with open('models/lin_reg.bin', 'wb') as f_out:
+    pickle.dump((dv, lr), f_out)
+
+with mlflow.start_run():
+    mlflow.set_tag("developer", "daniel")
+
+    mlflow.log_param("train-data-path", "./data/yellow_tripdata_2021-01.csv")
+    mlflow.log_param("valid-data-path", "./data/yellow_tripdata_2021-02.csv")
+
+    alpha = 0.1
+    mlflow.log_param("alpha", alpha)
+    lr = Lasso(alpha)
+    lr.fit(X_train, y_train)
+
+    y_pred = lr.predict(X_val)
+    rmse = mean_squared_error(y_val, y_pred, squared=False)
+    mlflow.log_metric("rmse", rmse)
+
+    mlflow.log_artifact(local_path="models/lin_reg.bin", artifact_path="models_pickle")
+
+train = xgb.DMatrix(X_train, label=y_train)
+valid = xgb.DMatrix(X_val, label=y_val)
+
+
+def objective(params):
+    with mlflow.start_run():
+        mlflow.set_tag("model", "xgboost")
+        mlflow.log_params(params)
+        booster = xgb.train(
+            params=params,
+            dtrain=train,
+            num_boost_round=1000,
+            evals=[(valid, 'validation')],
+            early_stopping_rounds=50
+        )
+        y_pred = booster.predict(valid)
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
+        mlflow.log_metric("rmse", rmse)
+
+    return {'loss': rmse, 'status': STATUS_OK}
+
+
+search_space = {
+    'max_depth': scope.int(hp.quniform('max_depth', 4, 100, 1)),
+    'learning_rate': hp.loguniform('learning_rate', -3, 0),
+    'reg_alpha': hp.loguniform('reg_alpha', -5, -1),
+    'reg_lambda': hp.loguniform('reg_lambda', -6, -1),
+    'min_child_weight': hp.loguniform('min_child_weight', -1, 3),
+    'objective': 'reg:linear',
+    'seed': 42
+}
+
+best_result = fmin(
+    fn=objective,
+    space=search_space,
+    algo=tpe.suggest,
+    max_evals=50,
+    trials=Trials()
+)
+
+mlflow.sklearn.autolog()
+
+for model_class in (RandomForestRegressor, GradientBoostingRegressor, ExtraTreesRegressor, LinearSVR):
+    with mlflow.start_run():
+        mlflow.log_param("train-data-path", "./data/yellow_tripdata_2021-01.csv")
+        mlflow.log_param("valid-data-path", "./data/yellow_tripdata_2021-02.csv")
+        # mlflow.log_artifact("./models/preprocessor.b", artifact_path="preprocessor")
+
+        mlmodel = model_class()
+        mlmodel.fit(X_train, y_train)
+
+        y_pred = mlmodel.predict(X_val)
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
+        mlflow.log_metric("rmse", rmse)
---
+++ scripts.sh
@@ -0,0 +1,2 @@
+# launch mlflow
+mlflow ui --backend-store-uri sqlite:///mlflow/mlflow.db --default-artifact-root ./mlflow/mlruns
